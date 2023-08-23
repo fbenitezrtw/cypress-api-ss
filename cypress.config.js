0652f299c83ff824e9c748e78be1786a52f05abf
Fernando Benitez <fernandotee@gmail.com>
2023-08-23 15:35:38 -0300

    SymphonyS initial commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  e2e: {
+    baseUrl: 'https://api.publicapis.org/',
+  },
+  supportFolder: false
+})
